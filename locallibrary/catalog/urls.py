e0bc02c14b14c49a59e2e5317038f7bf7f9ef85c
ahmedralheliwy <ahmed.alheliwy0.99@gmail.com>
2024-12-30 13:09:35 +0200

    Cx: Somethings needs push

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -13,5 +13,6 @@ urlpatterns=[
         path('author/<int:pk>',views.AuthorDetailView.as_view(),name='author-detail'),
         path('mybooks/',views.LoanedBooksByUserListView.as_view(),name='my-borrowed'),
         path('borrowed_books/',views.BorrowedBooksListView.as_view(),name='borrowed'),
+        path('book/<uuid:pk>/renew/',views.renew_book_librarian,name='renew-book-librarian'),
 
         ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render 
+from django.shortcuts import render , get_object_or_404
 from .models import Book,Author,BookInstance,Genre,Language
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required,permission_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from catalog.forms import RenewBookForm 
+import datetime
 
 # Create your views here.
 
@@ -81,4 +85,24 @@ class BorrowedBooksListView(LoginRequiredMixin,PermissionRequiredMixin,generic.L
 
     def get_queryset(self):
         return (BookInstance.objects.filter(status__exact='o').order_by('due_back'))
+
+
+
+@login_required
+@permission_required('catalog.can_mark_returned',raise_exception=True)
+def renew_book_librarian(request,pk):
+    book_instance=get_object_or_404(BookInstance,pk=pk)
+    if request.method=='POST':
+        form=RenewBookForm(request.POST)
+        if form.is_valid():
+            book_instance.due_back=form.cleaned_data['renewal_date']
+            book_instance.save()
+            return HttpResponseRedirect(reverse('borrowed'))
+
+    else:
+        proposed_renewal_date=datetime.date.today() + datetime.timedelta(weeks=3)
+        form=RenewBookForm(initial={'renewal_date':proposed_renewal_date})
+        context={'form':form,'book_instance':book_instance}
+
+    return render(request,'catalog/book_renew_librarian.html',context)
 
